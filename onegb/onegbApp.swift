876dbbd72b9bf5bf5b1cf04fa14a815d858a8026
Ata <atakanozalan@gmail.com>
2023-12-11 18:46:23 +0300

    Initial Commit

---
+++ onegb/onegbApp.swift
@@ -0,0 +1,17 @@
+//
+//  onegbApp.swift
+//  onegb
+//
+//  Created by Ezagor on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct onegbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
