f33b947fa1d364a7343fe4a58fba477776e88eea
Ata <atakanozalan@gmail.com>
2023-12-15 17:46:34 +0300

    Integrating card home, walletview and onewalletview also contentview

---
+++ onegb/WalletEsentepeView.swift
@@ -0,0 +1,18 @@
+//
+//  WalletEsentepeView.swift
+//  onegb
+//
+//  Created by Ezagor on 15.12.2023.
+//
+
+import SwiftUI
+
+struct WalletEsentepeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WalletEsentepeView()
+}
